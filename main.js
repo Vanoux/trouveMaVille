3d9abf63e550899cc7e57e329b8f564c66d758e2
Vanoux <vanessa.roux891@orange.fr>
2018-06-01 14:27:38 +0200

    presque fini

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 function meteo() {
 
-$.getJSON("http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22",
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Toulouse,fr&appid=d23814f807c7a678102c27597bb36652&units=metric",
         function (data) {
-            var celsius = data.list[0].main.temp;
-            var descript = data.list[0].weather.description;
-            $(".contain").html("Température : " + celsius + " °C" + " Méteo : " + descript);
+            var celsius = data.main.temp;
+            var descript = data.weather[0].description;
+            $("#contain1").html("METEO TOULOUSE AUJOURD'HUI=> Température : " + celsius + " °C" + " Méteo : " + descript);
         });
 };
+
+
+function adresse() {
+
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Saint-gaudens,fr&appid=d23814f807c7a678102c27597bb36652&units=metric",
+        function (data) {
+            var latitude = data.coord.lat;
+            var longitude = data.coord.lon;
+            $("#contain2").html("SAINT GAUDENS => Latitude : " + latitude + " et  Longitude : " + longitude);
+        });
+};
+
+
+function itineraire() {
+    
+    $.getJSON("https://www.mapquestapi.com/directions/v2/route?key=ZV75MleoxvAZUhoxTiO9ApBKPrYOALiy&from=Toulouse&to=Saint-gaudens&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false",
+            function (data) {
+                var temps = data.route.legs[0].formattedTime;
+                var distance = data.route.legs[0].distance;
+                $("#contain3").html("TRAJET TOULOUSE / ST GAUDENS => Temps : " + temps + " et Distance : " + distance*1.6 + "Km");
+    });
+};
+
+
+
+
+
+//d23814f807c7a678102c27597bb36652 clé api openweather
+//ZV75MleoxvAZUhoxTiO9ApBKPrYOALiy clé api MapQuest
+
+//https://www.mapquestapi.com/directions/v2/route?key=KEY&from=Toulouse&to=Saint-gaudens&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false
+
